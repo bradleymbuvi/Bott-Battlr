f4d26a42b0cfd9f03e7f86a909c533fbbe46a44c
bradleymbuvi <bradwent@gmail.com>
2024-02-15 22:16:36 +0300

    Updated App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import BotsPage from "./containers/BotsPage"
+import "./App.css"
+
+
+export default function App() {
+    return (
+      <div className="App">
+        <BotsPage />
+      </div>
+    )
+  }
+  
+  export default App
